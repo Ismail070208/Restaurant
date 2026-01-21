30a2499aa55e42d39b0ff0aa6f018a3aebcd5796
Ismail Abdikov <ismailabdikov20@gmail.com>
2026-01-21 14:03:44 +0200

    подобрих навигацията, като остава за още малко и тази форма ще бъде завършена

--- Restaurant/NavigationForm.Designer.cs
+++ Restaurant/NavigationForm.Designer.cs
@@ -32,21 +32,24 @@
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelNavigation = new System.Windows.Forms.Panel();
-            this.btnUsers = new System.Windows.Forms.Button();
+            this.btnStatistics = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnAuthor = new System.Windows.Forms.Button();
-            this.btnReports = new System.Windows.Forms.Button();
             this.btnOrders = new System.Windows.Forms.Button();
             this.btnMenu = new System.Windows.Forms.Button();
+            this.btnUsers = new System.Windows.Forms.Button();
+            this.btnReports = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panelNavigation.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Stencil", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(317, 147);
+            this.label1.Location = new System.Drawing.Point(69, 53);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(335, 47);
             this.label1.TabIndex = 0;
@@ -55,7 +58,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(386, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(410, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(196, 115);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -64,108 +67,185 @@
             // 
             // panelNavigation
             // 
-            this.panelNavigation.BackColor = System.Drawing.Color.LightSlateGray;
-            this.panelNavigation.Controls.Add(this.btnUsers);
+            this.panelNavigation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panelNavigation.Controls.Add(this.btnStatistics);
             this.panelNavigation.Controls.Add(this.btnExit);
             this.panelNavigation.Controls.Add(this.btnAuthor);
-            this.panelNavigation.Controls.Add(this.btnReports);
             this.panelNavigation.Controls.Add(this.btnOrders);
             this.panelNavigation.Controls.Add(this.btnMenu);
-            this.panelNavigation.Location = new System.Drawing.Point(106, 238);
+            this.panelNavigation.Controls.Add(this.btnUsers);
+            this.panelNavigation.Controls.Add(this.btnReports);
+            this.panelNavigation.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelNavigation.Location = new System.Drawing.Point(0, 0);
             this.panelNavigation.Name = "panelNavigation";
-            this.panelNavigation.Size = new System.Drawing.Size(799, 270);
+            this.panelNavigation.Size = new System.Drawing.Size(277, 597);
             this.panelNavigation.TabIndex = 2;
+            this.panelNavigation.Paint += new System.Windows.Forms.PaintEventHandler(this.panelNavigation_Paint);
             // 
-            // btnUsers
+            // btnStatistics
             // 
-            this.btnUsers.BackColor = System.Drawing.Color.Cyan;
-            this.btnUsers.Font = new System.Drawing.Font("Cooper Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUsers.Location = new System.Drawing.Point(557, 156);
-            this.btnUsers.Name = "btnUsers";
-            this.btnUsers.Size = new System.Drawing.Size(191, 80);
-            this.btnUsers.TabIndex = 5;
-            this.btnUsers.Text = "Users";
-            this.btnUsers.UseVisualStyleBackColor = false;
-            this.btnUsers.Click += new System.EventHandler(this.btnUsers_Click);
+            this.btnStatistics.BackColor = System.Drawing.Color.Gray;
+            this.btnStatistics.FlatAppearance.BorderSize = 0;
+            this.btnStatistics.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStatistics.Font = new System.Drawing.Font("Cooper Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStatistics.ForeColor = System.Drawing.Color.White;
+            this.btnStatistics.Location = new System.Drawing.Point(0, 288);
+            this.btnStatistics.Name = "btnStatistics";
+            this.btnStatistics.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btnStatistics.Size = new System.Drawing.Size(277, 47);
+            this.btnStatistics.TabIndex = 6;
+            this.btnStatistics.Text = "Statistics";
+            this.btnStatistics.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnStatistics.UseVisualStyleBackColor = false;
+            this.btnStatistics.Click += new System.EventHandler(this.btnStatistics_Click);
+            this.btnStatistics.MouseEnter += new System.EventHandler(this.NavButton_MouseEnter);
+            this.btnStatistics.MouseLeave += new System.EventHandler(this.NavButton_MouseLeave);
             // 
             // btnExit
             // 
-            this.btnExit.BackColor = System.Drawing.Color.Cyan;
+            this.btnExit.BackColor = System.Drawing.Color.Gray;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnExit.Font = new System.Drawing.Font("Cooper Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.Location = new System.Drawing.Point(304, 156);
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(0, 543);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(191, 80);
+            this.btnExit.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btnExit.Size = new System.Drawing.Size(277, 47);
             this.btnExit.TabIndex = 4;
             this.btnExit.Text = "Exit";
+            this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnExit.UseVisualStyleBackColor = false;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            this.btnExit.MouseEnter += new System.EventHandler(this.NavButton_MouseEnter);
+            this.btnExit.MouseLeave += new System.EventHandler(this.NavButton_MouseLeave);
             // 
             // btnAuthor
             // 
-            this.btnAuthor.BackColor = System.Drawing.Color.Cyan;
+            this.btnAuthor.BackColor = System.Drawing.Color.Gray;
+            this.btnAuthor.FlatAppearance.BorderSize = 0;
+            this.btnAuthor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAuthor.Font = new System.Drawing.Font("Cooper Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAuthor.Location = new System.Drawing.Point(52, 156);
+            this.btnAuthor.ForeColor = System.Drawing.Color.White;
+            this.btnAuthor.Location = new System.Drawing.Point(0, 341);
             this.btnAuthor.Name = "btnAuthor";
-            this.btnAuthor.Size = new System.Drawing.Size(191, 80);
+            this.btnAuthor.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btnAuthor.Size = new System.Drawing.Size(277, 47);
             this.btnAuthor.TabIndex = 3;
             this.btnAuthor.Text = "Author";
+            this.btnAuthor.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnAuthor.UseVisualStyleBackColor = false;
             this.btnAuthor.Click += new System.EventHandler(this.btnAuthor_Click);
-            // 
-            // btnReports
-            // 
-            this.btnReports.BackColor = System.Drawing.Color.Cyan;
-            this.btnReports.Font = new System.Drawing.Font("Cooper Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReports.Location = new System.Drawing.Point(557, 34);
-            this.btnReports.Name = "btnReports";
-            this.btnReports.Size = new System.Drawing.Size(191, 80);
-            this.btnReports.TabIndex = 2;
-            this.btnReports.Text = "Reports";
-            this.btnReports.UseVisualStyleBackColor = false;
-            this.btnReports.Click += new System.EventHandler(this.btnReports_Click);
+            this.btnAuthor.MouseEnter += new System.EventHandler(this.NavButton_MouseEnter);
+            this.btnAuthor.MouseLeave += new System.EventHandler(this.NavButton_MouseLeave);
             // 
             // btnOrders
             // 
-            this.btnOrders.BackColor = System.Drawing.Color.Cyan;
+            this.btnOrders.BackColor = System.Drawing.Color.Gray;
+            this.btnOrders.FlatAppearance.BorderSize = 0;
+            this.btnOrders.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnOrders.Font = new System.Drawing.Font("Cooper Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOrders.Location = new System.Drawing.Point(304, 34);
+            this.btnOrders.ForeColor = System.Drawing.Color.White;
+            this.btnOrders.Location = new System.Drawing.Point(0, 234);
             this.btnOrders.Name = "btnOrders";
-            this.btnOrders.Size = new System.Drawing.Size(191, 80);
+            this.btnOrders.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btnOrders.Size = new System.Drawing.Size(277, 48);
             this.btnOrders.TabIndex = 1;
             this.btnOrders.Text = "Orders";
+            this.btnOrders.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnOrders.UseVisualStyleBackColor = false;
             this.btnOrders.Click += new System.EventHandler(this.btnOrders_Click);
+            this.btnOrders.MouseEnter += new System.EventHandler(this.NavButton_MouseEnter);
+            this.btnOrders.MouseLeave += new System.EventHandler(this.NavButton_MouseLeave);
             // 
             // btnMenu
             // 
-            this.btnMenu.BackColor = System.Drawing.Color.Cyan;
+            this.btnMenu.BackColor = System.Drawing.Color.Gray;
+            this.btnMenu.FlatAppearance.BorderSize = 0;
+            this.btnMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMenu.Font = new System.Drawing.Font("Cooper Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMenu.Location = new System.Drawing.Point(52, 34);
+            this.btnMenu.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnMenu.Location = new System.Drawing.Point(0, 180);
             this.btnMenu.Name = "btnMenu";
-            this.btnMenu.Size = new System.Drawing.Size(191, 80);
+            this.btnMenu.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btnMenu.Size = new System.Drawing.Size(277, 48);
             this.btnMenu.TabIndex = 0;
             this.btnMenu.Text = "Menu";
+            this.btnMenu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnMenu.UseVisualStyleBackColor = false;
             this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            this.btnMenu.Enter += new System.EventHandler(this.btnMenu_Enter);
+            this.btnMenu.MouseEnter += new System.EventHandler(this.NavButton_MouseEnter);
+            this.btnMenu.MouseLeave += new System.EventHandler(this.NavButton_MouseLeave);
+            // 
+            // btnUsers
+            // 
+            this.btnUsers.BackColor = System.Drawing.Color.Gray;
+            this.btnUsers.FlatAppearance.BorderSize = 0;
+            this.btnUsers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUsers.Font = new System.Drawing.Font("Cooper Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsers.ForeColor = System.Drawing.Color.White;
+            this.btnUsers.Location = new System.Drawing.Point(0, 410);
+            this.btnUsers.Name = "btnUsers";
+            this.btnUsers.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btnUsers.Size = new System.Drawing.Size(277, 47);
+            this.btnUsers.TabIndex = 5;
+            this.btnUsers.Text = "Users";
+            this.btnUsers.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnUsers.UseVisualStyleBackColor = false;
+            this.btnUsers.Click += new System.EventHandler(this.btnUsers_Click);
+            this.btnUsers.MouseEnter += new System.EventHandler(this.NavButton_MouseEnter);
+            this.btnUsers.MouseLeave += new System.EventHandler(this.NavButton_MouseLeave);
+            // 
+            // btnReports
+            // 
+            this.btnReports.BackColor = System.Drawing.Color.Gray;
+            this.btnReports.FlatAppearance.BorderSize = 0;
+            this.btnReports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReports.Font = new System.Drawing.Font("Cooper Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReports.ForeColor = System.Drawing.Color.White;
+            this.btnReports.Location = new System.Drawing.Point(0, 463);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btnReports.Size = new System.Drawing.Size(277, 47);
+            this.btnReports.TabIndex = 2;
+            this.btnReports.Text = "Reports";
+            this.btnReports.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReports.UseVisualStyleBackColor = false;
+            this.btnReports.Click += new System.EventHandler(this.btnReports_Click);
+            this.btnReports.MouseEnter += new System.EventHandler(this.NavButton_MouseEnter);
+            this.btnReports.MouseLeave += new System.EventHandler(this.NavButton_MouseLeave);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(277, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(804, 141);
+            this.panel1.TabIndex = 3;
             // 
             // NavigationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Plum;
-            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1081, 597);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.panelNavigation);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "NavigationForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Restaurant Manager";
+            this.Load += new System.EventHandler(this.NavigationForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panelNavigation.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -180,6 +260,8 @@
         private System.Windows.Forms.Button btnReports;
         private System.Windows.Forms.Button btnOrders;
         private System.Windows.Forms.Button btnUsers;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStatistics;
     }
 }
 
--- Restaurant/NavigationForm.cs
+++ Restaurant/NavigationForm.cs
@@ -13,12 +13,38 @@ namespace Restaurant
     public partial class NavigationForm : Form
     {
         private string userRole;
+        private Button activeButton = null;
 
         public NavigationForm(string role)
         {
             InitializeComponent();
             userRole = role;
             ApplyRolePermissions();
+        }
+
+        private void NavButton_MouseEnter(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (btn != activeButton)
+                btn.BackColor = Color.FromArgb(60, 110, 230);
+        }
+
+        private void NavButton_MouseLeave(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (btn != activeButton)
+                btn.BackColor = Color.FromArgb(40, 40, 40);
+        }
+
+        private void SetActiveButton(Button btn)
+        {
+            if (activeButton != null)
+                activeButton.BackColor = Color.FromArgb(40, 40, 40); // normal
+
+            activeButton = btn;
+            activeButton.BackColor = Color.FromArgb(60, 110, 230); // active
         }
 
         private void ApplyRolePermissions()
@@ -49,32 +75,76 @@ namespace Restaurant
 
         private void btnMenu_Click(object sender, EventArgs e)
         {
+            SetActiveButton((Button)sender);
             MenuForm form = new MenuForm();
-            form.Show();
+            form.ShowDialog();
         }
 
         private void btnOrders_Click(object sender, EventArgs e)
         {
+            SetActiveButton((Button)sender);
             OrdersForm form = new OrdersForm();
-            form.Show();
+            form.ShowDialog();
         }
 
         private void btnReports_Click(object sender, EventArgs e)
         {
+            SetActiveButton((Button)sender);
             ReportsForm form = new ReportsForm();
-            form.Show();
+            form.ShowDialog();
         }
 
         private void btnAuthor_Click(object sender, EventArgs e)
         {
+            SetActiveButton((Button)sender);
             AuthorForm frm = new AuthorForm();
-            frm.Show();
+            frm.ShowDialog();
         }
 
         private void btnUsers_Click(object sender, EventArgs e)
         {
+            SetActiveButton((Button)sender);
             UsersForm frm = new UsersForm();
-            frm.Show();
+            frm.ShowDialog();
+        }
+
+        private void panelNavigation_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void NavigationForm_Load(object sender, EventArgs e)
+        {
+            panelNavigation.BackColor = Color.FromArgb(40, 40, 40);
+            btnMenu.BackColor = Color.FromArgb(40, 40, 40);
+            btnOrders.BackColor = Color.FromArgb(40, 40, 40);
+            btnAuthor.BackColor = Color.FromArgb(40, 40, 40);
+            btnExit.BackColor = Color.FromArgb(40, 40, 40);
+            btnStatistics.BackColor = Color.FromArgb(40, 40, 40);
+            btnReports.BackColor = Color.FromArgb(40, 40, 40);
+            btnUsers.BackColor = Color.FromArgb(40, 40, 40);
+            //panel1.BackColor = Color.FromArgb(245, 245, 245);
+        }
+
+        private void btnMenu_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnMenu_MouseEnter(object sender, EventArgs e)
+        {
+        }
+
+        private void btnMenu_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStatistics_Click(object sender, EventArgs e)
+        {
+            SetActiveButton((Button)sender);
+            StatisticsForm frm = new StatisticsForm();
+            frm.ShowDialog();
         }
     }
 }
