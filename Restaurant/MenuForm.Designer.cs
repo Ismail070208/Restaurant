3c478f15df0ed864dbefa4f8e2b51109b2135026
Ismail Abdikov <ismailabdikov20@gmail.com>
2026-03-05 08:07:42 +0200

    something

--- Restaurant/MenuForm.Designer.cs
+++ Restaurant/MenuForm.Designer.cs
@@ -28,61 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panelAdmin = new System.Windows.Forms.Panel();
-            this.panelWaiter = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabAdmin = new System.Windows.Forms.TabPage();
+            this.tabWaiter = new System.Windows.Forms.TabPage();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.panelAdmin.SuspendLayout();
-            this.panelWaiter.SuspendLayout();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.tabAdmin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panelAdmin
+            // tabControl1
             // 
-            this.panelAdmin.Controls.Add(this.panelWaiter);
-            this.panelAdmin.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelAdmin.Location = new System.Drawing.Point(0, 0);
-            this.panelAdmin.Name = "panelAdmin";
-            this.panelAdmin.Size = new System.Drawing.Size(807, 458);
-            this.panelAdmin.TabIndex = 0;
-            this.panelAdmin.Paint += new System.Windows.Forms.PaintEventHandler(this.panelAdmin_Paint);
+            this.tabControl1.Controls.Add(this.tabAdmin);
+            this.tabControl1.Controls.Add(this.tabWaiter);
+            this.tabControl1.Location = new System.Drawing.Point(-8, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(821, 467);
+            this.tabControl1.TabIndex = 0;
             // 
-            // panelWaiter
+            // tabAdmin
             // 
-            this.panelWaiter.Controls.Add(this.label1);
-            this.panelWaiter.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelWaiter.Location = new System.Drawing.Point(0, 0);
-            this.panelWaiter.Name = "panelWaiter";
-            this.panelWaiter.Size = new System.Drawing.Size(807, 458);
-            this.panelWaiter.TabIndex = 0;
+            this.tabAdmin.Controls.Add(this.panel1);
+            this.tabAdmin.Controls.Add(this.dgvProducts);
+            this.tabAdmin.Location = new System.Drawing.Point(4, 22);
+            this.tabAdmin.Name = "tabAdmin";
+            this.tabAdmin.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAdmin.Size = new System.Drawing.Size(813, 441);
+            this.tabAdmin.TabIndex = 0;
+            this.tabAdmin.Text = "tabAdmin";
+            this.tabAdmin.UseVisualStyleBackColor = true;
+            // 
+            // tabWaiter
+            // 
+            this.tabWaiter.Location = new System.Drawing.Point(4, 22);
+            this.tabWaiter.Name = "tabWaiter";
+            this.tabWaiter.Padding = new System.Windows.Forms.Padding(3);
+            this.tabWaiter.Size = new System.Drawing.Size(813, 441);
+            this.tabWaiter.TabIndex = 1;
+            this.tabWaiter.Text = "tabWaiter";
+            this.tabWaiter.UseVisualStyleBackColor = true;
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.AllowUserToAddRows = false;
+            this.dgvProducts.AllowUserToDeleteRows = false;
+            this.dgvProducts.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgvProducts.Location = new System.Drawing.Point(3, 3);
+            this.dgvProducts.MultiSelect = false;
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.ReadOnly = true;
+            this.dgvProducts.RowHeadersVisible = false;
+            this.dgvProducts.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProducts.Size = new System.Drawing.Size(807, 150);
+            this.dgvProducts.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cmbCategory);
+            this.panel1.Controls.Add(this.txtDescription);
+            this.panel1.Controls.Add(this.txtPrice);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(94, 215);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(530, 159);
+            this.panel1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(360, 75);
+            this.label1.Location = new System.Drawing.Point(29, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Price:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Description:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Category:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(74, 24);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(69, 50);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 5;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(98, 74);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(100, 20);
+            this.txtDescription.TabIndex = 6;
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(87, 100);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(121, 21);
+            this.cmbCategory.TabIndex = 7;
             // 
             // MenuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(807, 458);
-            this.Controls.Add(this.panelAdmin);
+            this.Controls.Add(this.tabControl1);
             this.Name = "MenuForm";
             this.Text = "MenuForm";
             this.Load += new System.EventHandler(this.MenuForm_Load);
-            this.panelAdmin.ResumeLayout(false);
-            this.panelWaiter.ResumeLayout(false);
-            this.panelWaiter.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabAdmin.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panelAdmin;
-        private System.Windows.Forms.Panel panelWaiter;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabAdmin;
+        private System.Windows.Forms.TabPage tabWaiter;
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ComboBox cmbCategory;
     }
 }
--- Restaurant/MenuForm.cs
+++ Restaurant/MenuForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -28,14 +29,65 @@ namespace Restaurant
         {
             if (role == "Admin")
             {
-                panelAdmin.Visible = true;
-                panelWaiter.Visible = false;
+                tabControl1.SelectedTab = tabAdmin;
+                LoadCategories();
+                LoadProducts();
             }
             else
             {
-                panelAdmin.Visible = false;
-                panelWaiter.Visible = true;
+                tabControl1.SelectedTab = tabWaiter;
             }
+
+            tabControl1.Appearance = TabAppearance.FlatButtons;
+            tabControl1.ItemSize = new Size(0, 1);
+            tabControl1.SizeMode = TabSizeMode.Fixed;
+        }
+
+        private void LoadProducts()
+        {
+            string connectionString = @"Data Source=DESKTOP-743IP50\MSSQLSERVER2022;Initial Catalog=Restaurant;Integrated Security=True;";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = @"
+            SELECT 
+                P.Id,
+                P.Name,
+                P.Price,
+                P.Description,
+                C.Name AS Category
+            FROM Products P
+            INNER JOIN Categories C ON P.CategoryId = C.Id";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                dgvProducts.DataSource = dt;
+            }
+        }
+
+        private void LoadCategories()
+        {
+            string connectionString = @"Data Source=DESKTOP-743IP50\MSSQLSERVER2022;Initial Catalog=Restaurant;Integrated Security=True;";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT Id, Name FROM Categories";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                cmbCategory.DataSource = dt;
+                cmbCategory.DisplayMember = "Name";
+                cmbCategory.ValueMember = "Id";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
